c26efc513fa95cfb786d45db3129fa4bd82c1fe3
Patrick Luan <patrickluan360@gmail.com>
2019-10-04 11:44:28 -0300

    Update index for contents layouts

--- index.php
+++ index.php
@@ -2,52 +2,19 @@
 <html lang="pt-br">
 <head>
 	<title>Park & Junior - Início</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- BOOTSTRAP 4: Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-	<!-- AWESOME Icons -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
-	<!-- GOOGLE Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Livvic&display=swap" rel="stylesheet">
-	
-	<link rel="stylesheet" href="/styles/estilo.css">
-	<script src="/jscript/principal.js"></script>
+	<!-- #HEAD -->
+	<?php include('layout/head.php'); ?>
 </head>
 <body>
 	<header>
 		<div id="headerLogo">
 			<a href="/"><img src="/img/logo.png" class="mx-auto d-block img-fluid"></a>
 		</div>
-		<nav class="navbar navbar-dark navbar-expand-sm justify-content-between sticky-top">
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#expandMenu">
-				<i class="fas fa-bars"></i>
-			</button>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#expandPesquisa">
-				<i class="fas fa-search"></i>
-			</button>
 
-			<ul class="navbar-nav collapse navbar-collapse" id="expandMenu">
-				<li class="nav-item active"><a href="/" class="nav-link"><i class="fas fa-home"></i> Início</a></li>
-				<li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-newspaper"></i> Artigos</a></li>
-				<li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-table"></i> Portfólio</a></li>
-				<li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-briefcase"></i> Serviços</a></li>
-				<li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-headset"></i> Contato</a></li>
-			</ul>
-
-			<form class="collapse navbar-collapse mt-2 mt-sm-0 col-lg-4 col-xl-3" id="expandPesquisa">
-			<div class="input-group">
-				<input type="text" class="form-control" name="pesquisa" placeholder="Está procurando algo?">
-				<div class="input-group-append">
-				<button class="btn btn-primary" type="submit"><i class="fas fa-search"></i></button>
-				</div>
-			</div>
-			</form>
-		</nav>
+		<!-- #NAVBAR -->
+		<?php include('layout/navbar.php'); ?>
+		
 		<div class="py-4 container" id="term">
 			<div class="card border-0 rounded-lg bg-dark text-white">
 				<div class="card-header px-3 pb-1">
@@ -190,28 +157,7 @@
 		</div>
 	</section>
 
-	<footer class="bg-dark p-2 text-white text-center">
-		<div class="container-fluid row">
-		<div class="col-sm-4">
-			<h2 class="border-bottom p-2 border-secondary">Park & Junior</h2>
-			<a href="https://github.com/parkejunior/parkejunior5" class="btn btn-light" target="_blank"><i class="fab fa-github"></i> GitHub</a>
-		</div>
-		<div class="col-sm-4">
-			<h2 class="border-bottom p-2 border-secondary">Redes Sociais</h2>
-			<a href="#" class="text-white btn btn-primary mb-1"><i class="fab fa-facebook-square"></i> Facebook</a>
-			<a href="#" class="text-white btn btn-info mb-1"><i class="fab fa-twitter"></i> Twitter</a>
-			<a href="#" class="text-white btn btn-danger mb-1"><i class="fab fa-youtube"></i> YouTube</a>
-		</div>
-		<div class="col-sm-4">
-			<h2 class="border-bottom p-2 border-secondary">Certificados</h2>
-			<i class="fab fa-html5"></i> HTML5<br>
-			<i class="fab fa-css3-alt"></i> CSS3<br>
-			<i class="fab fa-js-square"></i> JavaScript<br>
-			<i class="fab fa-bootstrap"></i> Boostrap 4<br>
-			<i class="fab fa-php"></i> PHP<br>
-		</div>
-		</div>
-		<div>Copyright <i class="fas fa-copyright"></i> 2012 / <?php echo date("Y"); ?> - Park & Junior</div>
-	</footer>
+	<!-- #FOOTER -->
+	<?php include('layout/footer.php'); ?>
 </body>
 </html>
